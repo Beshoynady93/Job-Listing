43c6a058b96e789658c17a8a089b2068d33630ee
Beshoynady93 <101524188+Beshoynady93@users.noreply.github.com>
2023-11-20 04:22:46 +0200

    Styling the filter section

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -19,57 +19,63 @@ const Filter = ({ filteredItems, setFilteredItems }) => {
   };
 
   return (
-    <section className="p-4 bg-neutral-50">
-      <h2 className="font-heading font-bold text-center">Choose your filter</h2>
-      <div className="p-2 flex justify-around items-center">
-        <div>
-          <button
-            className="bg-neutral-lightGrayishCyanBG font-bold text-primary p-2 rounded-full cursor-pointer disabled:cursor-not-allowed disabled:bg-slate-500"
-            onClick={(e) => chooseToFilter(e)}
-          >
-            Frontend
-          </button>
-          <button
-            onClick={(e) => removeItemFromFiltered(e)}
-            className="bg-primary text-white p-1 rounded-r-sm"
-            data-type="Frontend"
-          >
-            X
-          </button>
+    <section className="px-4 py-2 bg-neutral-50 rounded-md w-11/12 shadow-lg mx-auto -translate-y-8">
+      {/* <h2 className="font-heading font-bold text-center">Choose your filter</h2> */}
+      <div className="p-2 flex items-center font-heading">
+        <div className="flex flex-wrap gap-4">
+          <div>
+            <button
+              className="bg-neutral-lightGrayishCyanBG font-bold text-primary p-2 rounded-full cursor-pointer disabled:cursor-not-allowed disabled:bg-slate-500"
+              onClick={(e) => chooseToFilter(e)}
+            >
+              Frontend
+            </button>
+            <button
+              onClick={(e) => removeItemFromFiltered(e)}
+              className="bg-primary text-white px-2 py-1 rounded-r-sm font-bold"
+              data-type="Frontend"
+            >
+              X
+            </button>
+          </div>
+          <div>
+            <button
+              className="bg-neutral-lightGrayishCyanBG font-bold text-primary p-2 rounded-full cursor-pointer disabled:cursor-not-allowed disabled:bg-slate-500"
+              onClick={(e) => chooseToFilter(e)}
+            >
+              CSS
+            </button>
+            <button
+              className="bg-primary text-white px-2 py-1 font-bold rounded-r-sm"
+              data-type="CSS"
+              onClick={(e) => removeItemFromFiltered(e)}
+            >
+              X
+            </button>
+          </div>
+          <div>
+            <button
+              className="bg-neutral-lightGrayishCyanBG font-bold text-primary p-2 rounded-full cursor-pointer disabled:cursor-not-allowed disabled:bg-slate-500"
+              onClick={(e) => chooseToFilter(e)}
+            >
+              Javascript
+            </button>
+            <button
+              className="bg-primary text-white px-2 py-1 font-bold rounded-r-sm"
+              data-type="Javascript"
+              onClick={(e) => removeItemFromFiltered(e)}
+            >
+              X
+            </button>
+          </div>
         </div>
-        <div>
-          <button
-            className="bg-neutral-lightGrayishCyanBG font-bold text-primary p-2 rounded-full cursor-pointer disabled:cursor-not-allowed disabled:bg-slate-500"
-            onClick={(e) => chooseToFilter(e)}
-          >
-            CSS
-          </button>
-          <button
-            className="bg-primary text-white p-1 rounded-r-sm"
-            data-type="CSS"
-            onClick={(e) => removeItemFromFiltered(e)}
-          >
-            X
-          </button>
-        </div>
-        <div>
-          <button
-            className="bg-neutral-lightGrayishCyanBG font-bold text-primary p-2 rounded-full cursor-pointer disabled:cursor-not-allowed disabled:bg-slate-500"
-            onClick={(e) => chooseToFilter(e)}
-          >
-            Javascript
-          </button>
-          <button
-            className="bg-primary text-white p-1 rounded-r-sm"
-            data-type="Javascript"
-            onClick={(e) => removeItemFromFiltered(e)}
-          >
-            X
-          </button>
-        </div>
-        <button onClick={clearFilteredItemsHandler}>Clear</button>
+        <button
+          className="text-neutral-darkGrayishCyan font-bold"
+          onClick={clearFilteredItemsHandler}
+        >
+          Clear
+        </button>
       </div>
-      <div></div>
     </section>
   );
 };
